fa0f3c4c1c1f12b84cb81f6a66654e7b8e1ae4d0
Danrlei <danrleioliveira1792@gmail.com>
2023-02-27 21:49:22 -0300

    code in java

---
+++ src/TiposVariaveis.java
@@ -0,0 +1,13 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+        // para constante:
+        final double VALOR_DE_PI = 3.14;
+    }
+}
